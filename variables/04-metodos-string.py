7fd85a5ee045f9168c0f863e620e3a7fc4c47e06
Richard Marles <marleswebinfo@gmail.com>
2023-09-15 13:42:07 +0200

    feat: metodos o clases que podemos usar con strings

---
+++ variables/04-metodos-string.py
@@ -0,0 +1,12 @@
+animal = "  chaNHito fEliz  "
+print(animal.upper())
+print(animal.lower())
+print(animal.capitalize())
+print(animal.title())
+print(animal.strip())#quita los espacios a los dos lados de la cadena de texto
+print(animal.rstrip())#quita los espacios a la derecha de la cadena de texto
+print(animal.lstrip())#quita los espacios a la izquierda de la cadena de texto
+print(animal.find("ch"))#busca una cadena de caracteres
+print(animal.replace("Hi","puto"))#remplazar un cadena por otra
+print("cha" in animal)#si encuentra la cadena devuelve true si no false
+print("cha" not in animal)#si encuentra la cadena devuelve true si no false
