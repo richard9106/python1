81821312a15a367aff9f698bec34c9fee24bbb47
Richard Marles <marleswebinfo@gmail.com>
2023-09-15 13:18:37 +0200

    feat:how work the string in python

---
+++ variables/02-string.py
@@ -0,0 +1,12 @@
+nombre_curso= "Python inicia"
+description = """
+contiene todo lo que necesitas para
+convertirte en desarrollador de aplicaciones
+"""
+print (nombre_curso, ":", description)##imprime el valor de las variables
+print (len(nombre_curso))##muestra la cantidad de caracteres
+print (nombre_curso[2])##imprime un caracter de la cadena de texto como un array
+print (nombre_curso[0:6])##cortar una cadena de texto pide inicio y fin
+print (nombre_curso[5:])##cortar una cadena de texto si solo ponemos un valor desde ese hasta el final
+print (nombre_curso[:6])##cortar una cadena de texto al reves
+print (nombre_curso[:])##copia todo el string
