3b25954d063bdcc202c17e3b1c9d6ccb776b9af1
Richard Marles <marleswebinfo@gmail.com>
2023-09-15 13:03:47 +0200

    feat: adding Variable folder and 01variable file

---
+++ variables/01-variables.py
@@ -0,0 +1,15 @@
+"""No se puede iniciar variables iniciando con un numero"""
+
+nombre_curso = "Ultimate Python"
+nombre1="hola"
+nombre_Curso="Mundo"
+nombreCurso="feliz"
+print (nombre_curso)
+print (nombre1, nombre_Curso, nombreCurso)
+
+"""Numeros enteros y floats"""
+alumnos = 200
+puntaje = 9.9
+
+"""Booleanos"""
+publicado = True
