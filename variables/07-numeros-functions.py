2d7278d7dd41c388b6be3c0706256206064cddfe
Richard Marles <marleswebinfo@gmail.com>
2023-09-26 10:48:06 +0200

    feat:function and numbers

---
+++ variables/07-numeros-functions.py
@@ -0,0 +1,11 @@
+import math
+
+print (round(1.3))
+print (round(1.6))
+print (round(1.7))
+print (round(1.5))
+
+print (math.ceil(1.1))
+print (math.floor(1.99))
+print (math.isnan(23))
+# print (math.isnam("1.1"))
